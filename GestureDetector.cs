06217167f7fc6d23445f5df1293e4e846bab2097
henry78925 <henry78925@gmail.com>
2019-08-26 15:28:59 +0800

    no message

---
+++ GestureDetector.cs
@@ -0,0 +1,240 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="GestureDetector.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Microsoft.Samples.Kinect.DiscreteGestureBasics
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.Kinect;
+    using Microsoft.Kinect.VisualGestureBuilder;
+
+    /// <summary>
+    /// Gesture Detector class which listens for VisualGestureBuilderFrame events from the service
+    /// and updates the associated GestureResultView object with the latest results for the 'Seated' gesture
+    /// </summary>
+    public class GestureDetector : IDisposable
+    {
+        /// <summary> Path to the gesture database that was trained with VGB </summary>
+        private readonly string myGestureDatabase = @"Database\myGesture.gbd";
+
+        /// <summary> Name of the discrete gesture in the database that we want to track </summary>
+        private readonly string sittingGestureName = "Sitting";
+        private readonly string rockGestureName = "rock";
+        private readonly string headGestureName = "head";
+
+        /// <summary> Gesture frame source which should be tied to a body tracking ID </summary>
+        private VisualGestureBuilderFrameSource vgbFrameSource = null;
+
+        /// <summary> Gesture frame reader which will handle gesture events coming from the sensor </summary>
+        private VisualGestureBuilderFrameReader vgbFrameReader = null;
+
+        /// <summary>
+        /// Initializes a new instance of the GestureDetector class along with the gesture frame source and reader
+        /// </summary>
+        /// <param name="kinectSensor">Active sensor to initialize the VisualGestureBuilderFrameSource object with</param>
+        /// <param name="gestureResultView">GestureResultView object to store gesture results of a single body to</param>
+        public GestureDetector(KinectSensor kinectSensor, GestureResultView gestureResultView)
+        {
+            if (kinectSensor == null)
+            {
+                throw new ArgumentNullException("kinectSensor");
+            }
+
+            if (gestureResultView == null)
+            {
+                throw new ArgumentNullException("gestureResultView");
+            }
+            
+            this.GestureResultView = gestureResultView;
+            
+            // create the vgb source. The associated body tracking ID will be set when a valid body frame arrives from the sensor.
+            this.vgbFrameSource = new VisualGestureBuilderFrameSource(kinectSensor, 0);
+            this.vgbFrameSource.TrackingIdLost += this.Source_TrackingIdLost;
+
+            // open the reader for the vgb frames
+            this.vgbFrameReader = this.vgbFrameSource.OpenReader();
+            if (this.vgbFrameReader != null)
+            {
+                this.vgbFrameReader.IsPaused = true;
+                this.vgbFrameReader.FrameArrived += this.Reader_GestureFrameArrived;
+            }
+
+            // load the 'Seated' gesture from the gesture database
+            using (VisualGestureBuilderDatabase database = new VisualGestureBuilderDatabase(this.myGestureDatabase))
+            {
+                // we could load all available gestures in the database with a call to vgbFrameSource.AddGestures(database.AvailableGestures), 
+                // but for this program, we only want to track one discrete gesture from the database, so we'll load it by name
+                foreach (Gesture gesture in database.AvailableGestures)
+                {
+                    if (   gesture.Name.Equals(this.sittingGestureName) 
+                        || gesture.Name.Equals(this.rockGestureName) 
+                        || gesture.Name.Equals(this.headGestureName) )
+                    {
+                        this.vgbFrameSource.AddGesture(gesture);
+                    }
+                }
+            }
+        }
+
+        /// <summary> Gets the GestureResultView object which stores the detector results for display in the UI </summary>
+        public GestureResultView GestureResultView { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the body tracking ID associated with the current detector
+        /// The tracking ID can change whenever a body comes in/out of scope
+        /// </summary>
+        public ulong TrackingId
+        {
+            get
+            {
+                return this.vgbFrameSource.TrackingId;
+            }
+
+            set
+            {
+                if (this.vgbFrameSource.TrackingId != value)
+                {
+                    this.vgbFrameSource.TrackingId = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether or not the detector is currently paused
+        /// If the body tracking ID associated with the detector is not valid, then the detector should be paused
+        /// </summary>
+        public bool IsPaused
+        {
+            get
+            {
+                return this.vgbFrameReader.IsPaused;
+            }
+
+            set
+            {
+                if (this.vgbFrameReader.IsPaused != value)
+                {
+                    this.vgbFrameReader.IsPaused = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Disposes all unmanaged resources for the class
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Disposes the VisualGestureBuilderFrameSource and VisualGestureBuilderFrameReader objects
+        /// </summary>
+        /// <param name="disposing">True if Dispose was called directly, false if the GC handles the disposing</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (this.vgbFrameReader != null)
+                {
+                    this.vgbFrameReader.FrameArrived -= this.Reader_GestureFrameArrived;
+                    this.vgbFrameReader.Dispose();
+                    this.vgbFrameReader = null;
+                }
+
+                if (this.vgbFrameSource != null)
+                {
+                    this.vgbFrameSource.TrackingIdLost -= this.Source_TrackingIdLost;
+                    this.vgbFrameSource.Dispose();
+                    this.vgbFrameSource = null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handles gesture detection results arriving from the sensor for the associated body tracking Id
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void Reader_GestureFrameArrived(object sender, VisualGestureBuilderFrameArrivedEventArgs e)
+        {
+            VisualGestureBuilderFrameReference frameReference = e.FrameReference;
+            using (VisualGestureBuilderFrame frame = frameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    // get the discrete gesture results which arrived with the latest frame
+                    IReadOnlyDictionary<Gesture, DiscreteGestureResult> discreteResults = frame.DiscreteGestureResults;
+
+                    if (discreteResults != null)
+                    {
+                        double[] Confidence = new double[] { 0, 0, 0 };
+
+                        int tempstate = 0, temphand = 0;
+
+                        DiscreteGestureResult result = null;
+                        // we only have one gesture in this source object, but you can get multiple gestures
+                        foreach (Gesture gesture in this.vgbFrameSource.Gestures)
+                       {
+                            discreteResults.TryGetValue(gesture, out result);
+
+                            if (result == null)
+                                continue;
+
+                            if (gesture.Name.Equals(this.sittingGestureName))
+                            {
+                                Confidence[0] = result.Confidence;//001
+                            }
+                            if (gesture.Name.Equals(this.rockGestureName))
+                            {
+                                Confidence[1] = result.Confidence;//010
+                            }
+                            if (gesture.Name.Equals(this.headGestureName))
+                            {
+                                Confidence[2] = result.Confidence;//100
+                            }
+                        }
+
+                        if (Confidence[0] < 0.8)//stand
+                        {
+                            Confidence[0] = 1;
+                        }
+                        else//sitting
+                        {
+                            tempstate = 1;
+                        }
+
+                        if (Confidence[1] > 0.7)
+                        {
+                            temphand = 2;
+                        }
+                        else if (Confidence[2] > 0.7)
+                        {
+                            temphand = 4;
+                        }
+
+                        // update the GestureResultView object with new gesture result values
+                        if (result != null)
+                            this.GestureResultView.UpdateGestureResult(true, true, (Confidence[0] + Confidence[temphand / 2]) / 2, tempstate + temphand);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handles the TrackingIdLost event for the VisualGestureBuilderSource object
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void Source_TrackingIdLost(object sender, TrackingIdLostEventArgs e)
+        {
+            // update the GestureResultView object to show the 'Not Tracked' image in the UI
+            this.GestureResultView.UpdateGestureResult(false, false, 0.0f, 0);
+        }
+    }
+}
